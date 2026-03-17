9433e1b480b6f8cc9b15f92be5a6c6a82dc4ad9b
Jerome T <jerometmail@gmail.com>
2026-03-17 14:26:47 -0400

    database

---
+++ persola/db_models.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .db import Base
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+
