5538d2b576f1d14b03219be3d4238588aa637bd3
Jerome T <jerometmail@gmail.com>
2026-04-01 10:50:38 -0400

    analysis engine

--- persola/__init__.py
+++ persola/__init__.py
@@ -1,10 +1,14 @@
 __version__ = "0.1.0"
 
+from persola.analysis import StyleAnalysis, StyleToKnobMapper, WritingStyleExtractor
 from persola.models import PersonaProfile, AgentConfig, DEFAULT_PRESETS, KNOB_DEFINITIONS, PresetName
 from persola.engine import PersonaEngine, SamplingCompiler
 
 __all__ = [
     "__version__",
+    "StyleAnalysis",
+    "StyleToKnobMapper",
+    "WritingStyleExtractor",
     "PersonaProfile",
     "AgentConfig", 
     "DEFAULT_PRESETS",
